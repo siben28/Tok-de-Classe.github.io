07a759a38c5e71d96274ba032c281953a3b8db83
siben28 <mariasiben48@gmail.com>
2024-12-26 15:01:29 -0300

    Add files via upload

---
+++ tokdeclasse/script.js
@@ -0,0 +1,11 @@
+// Animação de rolagem suave para as seções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
